15d2971613d50e92aa87ce99fcbc0994c8496135
Garrett Chitwood <gman@acvwa.com>
2025-10-16 12:39:22 -0700

    First commit draft, fixed a few issues, will fix more compile time errors soon.

--- ProblematicProblem/Program.cs
+++ ProblematicProblem/Program.cs
@@ -1,27 +1,29 @@
-System;
-using System.Collections.Generic
+using System;
+using System.Collections.Generic;
 using System.Threading;
 
-ProblematicProblem
+namespace ProblematicProblem
 {
-    Program class
+    public class Program
     {
-        Random rng;
-static bool cont = true;
-static List<string> activities = new List<string>() { "Movies", "Paintball", "Bowling", "Lazer Tag", "LAN Party", "Hiking", "Axe Throwing", "Wine Tasting" }
+        public static Random rng = new Random();
+        public static bool cont = true;
+        
+        public static List<string> activities = new List<string>()
+            { "Movies", "Paintball", "Bowling", "Lazer Tag", "LAN Party", "Hiking", "Axe Throwing", "Wine Tasting" };
         static void Main(string[] args)
-{
-    Console.Write("Hello, welcome to the random activity generator! \nWould you like to generate a random activity? yes/no: ")
-            bool cont = bool.Parse(Console.ReadLine());
-    Console.WriteLine();
-    Console.Write("We are going to need your information first! What is your name? ");
-    string userName = Console.ReadLine();
-    Console.WriteLine();
-    Console.Write("What is your age? ");
-    int userAge = Console.ReadLine();
-    Console.WriteLine();
-    Console.Write("Would you like to see the current list of activities? Sure/No thanks: ");
-    bool seeList = bool.Parse(Console.ReadLine());
+        { 
+            Console.Write("Hello, welcome to the random activity generator! \nWould you like to generate a random activity? yes/no: ");
+                cont = bool.Parse(Console.ReadLine());
+            Console.WriteLine();
+            Console.Write("We are going to need your information first! What is your name? ");
+            string userName = Console.ReadLine();
+            Console.WriteLine();
+            Console.Write("What is your age? ");
+            int userAge = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            Console.Write("Would you like to see the current list of activities? Sure/No thanks: ");
+            bool seeList = bool.Parse(Console.ReadLine());
     if (seeList)
     {
         foreach (string activity in activities)
@@ -29,6 +31,7 @@ static List<string> activities = new List<string>() { "Movies", "Paintball", "Bo
             Console.Write($"{activity} ");
             Thread.Sleep(250);
         }
+        
         Console.WriteLine();
         Console.Write("Would you like to add any activities before we generate one? yes/no: ");
         bool addToList = bool.Parse(Console.ReadLine());
@@ -57,7 +60,8 @@ static List<string> activities = new List<string>() { "Movies", "Paintball", "Bo
             Console.Write(". ");
             Thread.Sleep(500);
         }
-        Console.WriteLine()
+
+        Console.WriteLine();
                 Console.Write("Choosing your random activity");
         for (int i = 0; i < 9; i++)
         {
