b58cf15e12664d543ab1442fa8fea9ee01426555
Garrett Chitwood <gman@acvwa.com>
2025-10-19 15:51:54 -0700

    Runtime errors debugged, next commit will be logic errors.

--- ProblematicProblem/Program.cs
+++ ProblematicProblem/Program.cs
@@ -14,44 +14,81 @@ namespace ProblematicProblem
         static void Main(string[] args)
         { 
             Console.Write("Hello, welcome to the random activity generator! \nWould you like to generate a random activity? yes/no: ");
-                cont = bool.Parse(Console.ReadLine());
+            var userResponse = Console.ReadLine().ToLower();
+
+            while (userResponse != "yes" && userResponse != "no")
+            {
+                Console.Write("Error, please try again with your answer being Yes or No.");
+                userResponse = Console.ReadLine().ToLower();
+            }
+            
+            if (userResponse == "no")
+            {
+                Console.WriteLine("Understood, have a good day");
+                return;
+            }
+            
             Console.WriteLine();
             Console.Write("We are going to need your information first! What is your name? ");
             string userName = Console.ReadLine();
             Console.WriteLine();
             Console.Write("What is your age? ");
-            int userAge = int.Parse(Console.ReadLine());
-            Console.WriteLine();
-            Console.Write("Would you like to see the current list of activities? Sure/No thanks: ");
-            bool seeList = bool.Parse(Console.ReadLine());
-    if (seeList)
-    {
-        foreach (string activity in activities)
-        {
-            Console.Write($"{activity} ");
-            Thread.Sleep(250);
-        }
-        
-            Console.WriteLine();
-            Console.Write("Would you like to add any activities before we generate one? yes/no: ");
-            bool addToList = bool.Parse(Console.ReadLine());
-            Console.WriteLine();
-            while (addToList)
-        {
-            Console.Write("What would you like to add? ");
-            string userAddition = Console.ReadLine();
-            activities.Add(userAddition);
-            foreach (string activity in activities)
+            int userAge;
+            while (!int.TryParse(Console.ReadLine(), out userAge))
             {
-                Console.Write($"{activity} ");
-                Thread.Sleep(250);
+                Console.Write("Error, please try again with an age in numerical form: ");
             }
             
             Console.WriteLine();
-            Console.WriteLine("Would you like to add more? yes/no: ");
-            addToList = bool.Parse(Console.ReadLine());
+            Console.Write("Would you like to see the current list of activities? Sure/No thanks: ");
+            userResponse = Console.ReadLine().ToLower();
+
+            while (userResponse != "sure" && userResponse != "no thanks")
+            {
+                Console.Write("Error, please try again with your answer as Sure or No thanks: ");
+                userResponse = Console.ReadLine().ToLower();
+            }
+            
+            if (userResponse == "sure") 
+            { 
+                foreach (string activity in activities) 
+                { 
+                    Console.Write($"{activity} "); 
+                    Thread.Sleep(250); 
+                }
+        
+            Console.WriteLine();
+            Console.Write("Would you like to add any activities before we generate one? yes/no: ");
+            userResponse = Console.ReadLine().ToLower();
+
+            while (userResponse != "yes" && userResponse != "no")
+            {
+                Console.Write("Error, please try again with an answer of Yes or No: ");
+                userResponse = Console.ReadLine().ToLower();
+            }
+            
+            Console.WriteLine();
+            while (userResponse == "yes") 
+            { 
+                Console.Write("What would you like to add? "); 
+                string userAddition = Console.ReadLine(); 
+                activities.Add(userAddition); 
+                foreach (string activity in activities) 
+                { 
+                    Console.Write($"{activity} "); 
+                    Thread.Sleep(250); 
+                } 
+                Console.WriteLine(); 
+                Console.WriteLine("Would you like to add more? yes/no: "); 
+                userResponse = Console.ReadLine().ToLower();
+
+                while (userResponse != "yes" && userResponse != "no")
+                {
+                    Console.Write("Error, please try again with an answer of Yes or No: ");
+                    userResponse = Console.ReadLine().ToLower();
+                }
+            }
         }
-    }
 
     while (cont)
     {
@@ -82,9 +119,20 @@ namespace ProblematicProblem
             randomActivity = activities[randomNumber];
         }
 
-        Console.Write($"Ah got it! {randomActivity}, your random activity is: {userName}! Is this ok or do you want to grab another activity? Keep/Redo: ");
-                Console.WriteLine();
-        bool cont = bool.Parse(Console.ReadLine());
+        Console.Write($"Ah got it! {userName}, your random activity is: {randomActivity}! Is this ok or do you want to grab another activity? Keep/Redo: ");
+        Console.WriteLine();
+        userResponse = Console.ReadLine().ToLower();
+
+        while (userResponse != "keep" && userResponse != "redo")
+        {
+            Console.Write("Error, answer again with Keep or Redo: ");
+            userResponse = Console.ReadLine().ToLower();
+        }
+
+        if (userResponse == "keep")
+        {
+            cont = false;
+        }
     }
 }
     }
